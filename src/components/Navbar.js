d651fcfb29085ac8f0264e5e04b4f9c4359dbe3f
Rick H Nguyen <rick248nguyen@gmail.com>
2023-12-05 19:28:50 -0500

    delete extra files and download react-router-dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+    // Only for hiding buttons when shrink window
+    /*
+    const [openLinks, setOpenLinks] = useState(false);
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    }
+    */
+
+    return (
+        <div className="navbar">
+            
+        </div>
+    );
+}
