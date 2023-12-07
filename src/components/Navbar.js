9eb1d7e854379f8b87e140dfac83de3d9aee5d59
Rick H Nguyen <rick248nguyen@gmail.com>
2023-12-07 14:13:55 -0500

    remove contact page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,6 @@ const Navbar = () => {
                 <Link to="/">Home</Link>
                 <Link to="about">About</Link>
                 <Link to="projects">Projects</Link>
-                <Link to="contact">Contact</Link>
             </div>
         </nav>
     );
